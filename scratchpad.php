7c543586867f586dc20e4b4b34c87a2267821ce9
kem226 <kemurray2011@aol.com>
2015-02-18 23:05:55 -0500

    deregister the editor-expand to run our copy of the file with changes

--- scratchpad.php
+++ scratchpad.php
@@ -11,8 +11,10 @@
 add_action( 'admin_init', 'scratchpad_init' );
 
 function scratchpad_init() {
+    wp_dequeue_script('editor-expand');
+    wp_deregister_script('editor-expand');
     wp_enqueue_script( 'editor-expand-scratchpad', plugins_url( 'expand-editor-scratchpad.js', __FILE__ ),
-        array( 'jquery' , 'editor-expand') );
+        array( 'jquery') );
     wp_enqueue_style( 'scratchpad_css', plugins_url( 'scratchpad.css', __FILE__ ));
 }
 
